d39338c8bd6d7a6cdba890543ce0e3eb217afdb6
jamon.ben@gmail.com <jamon.ben@gmail.com>
2010-02-26 08:38:29 -0700

    Improved inputbox

--- src/chaos_cs/CsBifurcation/CsBifurcation/InputBox.cs
+++ src/chaos_cs/CsBifurcation/CsBifurcation/InputBox.cs
@@ -62,41 +62,65 @@ namespace CsBifurcation
         #region Windows Form Designer generated code
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.txtMessage = new System.Windows.Forms.TextBox();
-            label1.Location = new System.Drawing.Point(12, 8);
-            label1.Text = "Type in your message.";
-            label1.Size = new System.Drawing.Size(240, 48);
-            label1.TabIndex = 1;
-
-            btnOK.Location = new System.Drawing.Point(16, 104);
-            btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            btnOK.Size = new System.Drawing.Size(96, 24);
-            btnOK.TabIndex = 2;
-            btnOK.Text = "OK";
-            btnOK.Click += new System.EventHandler(this.btnOK_Click);
-            btnCancel.Location = new System.Drawing.Point(152, 104);
-            btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            btnCancel.Size = new System.Drawing.Size(96, 24);
-            btnCancel.TabIndex = 3;
-            btnCancel.Text = "Cancel";
-            txtMessage.Location = new System.Drawing.Point(16, 72);
-            txtMessage.TabIndex = 0;
-            txtMessage.Size = new System.Drawing.Size(232, 20);
-            this.Text = "Some Custom Dialog";
-            this.MaximizeBox = false;
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Type in your message.";
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(102, 104);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(70, 24);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(178, 104);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(70, 24);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(16, 72);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(232, 20);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // InputBoxForm
+            // 
+            this.AcceptButton = this.btnOK;
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(260, 139);
             this.ControlBox = false;
-            this.MinimizeBox = false;
-            this.ClientSize = new System.Drawing.Size(266, 151);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InputBoxForm";
+            this.Text = "Some Custom Dialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
         #endregion
 
