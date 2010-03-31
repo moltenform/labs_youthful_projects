015d931ba3ecaabb25f5c03ccd1fb06f406bfcb2
jamon.ben@gmail.com <jamon.ben@gmail.com>
2010-03-31 09:15:32 -0700

    Disable warning about unused.

--- src/chaos_cs/CsGeneralBitmap/CsGeneralBitmap/ScratchClass.cs
+++ src/chaos_cs/CsGeneralBitmap/CsGeneralBitmap/ScratchClass.cs
@@ -6,9 +6,10 @@ namespace CsGeneralBitmap
 {
     public class ScratchClass
     {
-        double val,fx,fy;
+        #pragma warning disable 0169 //disable warning about unused fields.
+        double val, fx, fy;
         bool bSwitch;
-        double c1,c2,c3,c4;
+        double c1, c2, c3, c4;
         double X0, X1, Y0, Y1;
         int paramSettle, paramIters;
         bool bIsRendering;
@@ -16,10 +17,12 @@ namespace CsGeneralBitmap
         Random R;
 
         int width; int height; double[] arrAns;
+        #pragma warning restore 0169
+
         public void getData()
         {
             //$$SECTION$$
-            
+
             //$$SECTION$$
         }
 
