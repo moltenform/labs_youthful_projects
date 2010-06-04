44b3f13dc2a5e4f518ac46ad6a8ea91b57c7db7e
jamon.ben@gmail.com <jamon.ben@gmail.com>
2010-06-03 19:22:04 -0700

    Using sse vector

--- src/chaos_cs/sdl_explore/sdl_burger_explorer/sdl/sdl_explore/menagerie.cpp
+++ src/chaos_cs/sdl_explore/sdl_burger_explorer/sdl/sdl_explore/menagerie.cpp
@@ -90,7 +90,8 @@ typedef struct
 } MenagerieSettings;
 //if symmetrical, we could make phasey0 0, but that wouldn't really be more efficient.
 
-MenagerieSettings mSettings = {5,150,16, 384,384, -3,1,-3,3};
+//MenagerieSettings mSettings = {5,150,16, 384,384, -3,1,-3,3};
+MenagerieSettings mSettings = {10,150,16, 384,384, -10,10,-10,10};
 MenagerieSettings mSettingsHigher = {5,150,16, 384,384, -3,1,-3,3};
 
 
@@ -109,14 +110,48 @@ MenagerieSettings mSettingsHigher = {5,150,16, 384,384, -3,1,-3,3};
 MenagerieSettings* managerieSettings = &mSettings;
 MenagerieSettings* managerieSettingsHigher = &mSettingsHigher;
 
+inline double _getLyapunov(MenagerieSettings* settings, double c1, double c2, int arrAns[])
+{
+	//http://sprott.physics.wisc.edu/chaos/lyapexp.htm
+	//double d0 = 1e-12;
+	double d0 = 1e-3, d1;
+	int N = 200; int settle = 20; //used to be 20
+	double total = 0.0;
+	double sx = -1.0, sy = -1.5;
+	double x=sx, y=sy, x_;
+	double x2=sx, y2=sy+d0, x2_;
+	for (int i=0; i<N; i++)
+	{
+		//x_ = 1 - c1*x*x + y; y = c2*x; 
+		//x=x_;
+		//x2_ = 1 - c1*x2*x2 + y2; y2 = c2*x2; 
+		//x2=x2_;
+		x_ = c1*x - y*y; y= c2*y + x*y;
+		x=x_;
+		x2_ = c1*x2 - y2*y2; y2= c2*y2 + x2*y2;
+		x2=x2_;
+
+		d1 = sqrt( (x2-x)*(x2-x) + (y2-y)*(y2-y) ); //distance
+		x2=x+ (d0/d1)*(x2-x); //also looks interesting when these are commented out
+		y2=y+ (d0/d1)*(y2-y);
+		if (i>settle) total+= log(d1/d0 );
+		if (ISTOOBIG(x) || ISTOOBIG(y)) return -300;
+	}
+	return total / (N-settle);
+}
+
+
+
+int CURRENTID=2;
 int _drawPhasePortrait(MenagerieSettings* settings, double c1, double c2, int arrAns[])
 {
+	CURRENTID++;
 	double sx,sy; int ii;//loop vars
 	int totalUniquePoints = 0;
 	int arrWidth = settings->phaseFigureWidth;
 	int arrHeight = settings->phaseFigureHeight;
 	//clear array
-	memset(arrAns, 0, sizeof(int)*arrWidth*arrHeight);
+	//memset(arrAns, 0, sizeof(int)*arrWidth*arrHeight); DON"T DO THIS it's SLOW!!!
 	double X0=settings->phasex0, X1=settings->phasex1, Y0=settings->phasey0, Y1=settings->phasey1;
 	double x, y, x_;
 	
@@ -130,31 +165,38 @@ int _drawPhasePortrait(MenagerieSettings* settings, double c1, double c2, int ar
 	{
 			x = sx; y=sy;
 
-			for (ii=0; ii<(settings->settling); ii++)
+			for (ii=0; ii<(settings->settling/4); ii++)
 			{
+				MAPEXPRESSION;
+				x = x_;
+				MAPEXPRESSION;
+				x = x_;
+				MAPEXPRESSION;
+				x = x_;
 				MAPEXPRESSION;
 				x = x_;
 				if ((ISTOOBIG(x)||ISTOOBIG(y))) break;
 			}
 			for (ii=0; ii<(settings->drawing); ii++)
 			{
+				if ((ISTOOBIG(x)||ISTOOBIG(y))) break;
 				MAPEXPRESSION;
 				x = x_;
-				if ((ISTOOBIG(x)||ISTOOBIG(y))) break;
 
 				int px = (int)(arrWidth * ((x - X0) / (X1 - X0)));
 				int py = (int)(arrHeight - arrHeight * ((y - Y0) / (Y1 - Y0)));
 				if (py >= 0 && py < arrHeight && px>=0 && px<arrWidth)
-				    if (arrAns[py + px * arrHeight]==0)
-				    { arrAns[py + px * arrHeight]=1; totalUniquePoints++;}
+				    if (arrAns[py + px * arrHeight]!=CURRENTID)
+				    { arrAns[py + px * arrHeight]=CURRENTID; totalUniquePoints++;}
 			}
 	}
 	}
 	return totalUniquePoints;
 }
+#include "fastmenag.h"
 
 
-
+#define LOWQSTEP 1
 int *arr=NULL;
 void DrawMenagerieLowQuality( SDL_Surface* pSmallSurface, PhasePortraitSettings*settings) 
 {
@@ -173,23 +215,46 @@ void DrawMenagerieLowQuality( SDL_Surface* pSmallSurface, PhasePortraitSettings*
     double dx = (X1 - X0) / width, dy = (Y1 - Y0) / height;
     fx = X0; fy = Y1; //y counts downwards
 	 char* pPosition;
-    for (int px = 0; px < width; px+=4)
+    for (int px = 0; px < width; px+=LOWQSTEP)
     {
         fy = Y1;
-        for (int py=0; py<height; py+=4)
+        for (int py=0; py<height; py+=LOWQSTEP)
         {
-		int hits= _drawPhasePortrait(managerieSettings, fx,fy,arr);
+		Uint32 r,g,b;
+
+		//int hits= _drawPhasePortrait(managerieSettings, fx,fy,arr);
+		int hits= _drawPhasePortraitVector(managerieSettings, fx,fy,arr);
 		double val = sqrt((double)hits)/10;// / 20.0;
 		if (val>1.0) val=1.0; if (val<0.0) val=0.0;
 		val = val*2 - 1; //from -1 to 1
-		Uint32 r,g,b;
 		if (val>=0)
 			b=255, r=g= (Uint32) ((1-val)*255.0);
 		else
 			r=g=b= (Uint32) ((val+1)*255.0);
+
+			/*
+			Lyapunov coloring::
+			double ly = _getLyapunov(managerieSettings, fx,fy,arr);
+			if (ly==-300) { r=0, g=60, b=0;
+			} else {
+		double val = ly ; ///5;
+				if (val>0) val = sqrt(((val)));
+				else val = -sqrt((-val));
+				val /= 1;
+		
+		/*int hits= _drawPhasePortrait(managerieSettings, fx,fy,arr);
+		double val = sqrt((double)hits)/10;// / 20.0; 
+
+		if (val>1.0) val=1.0; if (val<-1.0) val=-1.0;
+
+		if (val>=0)
+			b=255, r=g= (Uint32) ((1-val)*255.0);
+		else
+			r=g=b= (Uint32) ((val+1)*255.0);
+		}*/
   
-for (int ncy=0; ncy<4; ncy++){
-for (int ncx=0; ncx<4; ncx++){
+for (int ncy=0; ncy<LOWQSTEP; ncy++){
+for (int ncx=0; ncx<LOWQSTEP; ncx++){
 
   pPosition = ( char* ) pSmallSurface->pixels ; //determine position
   pPosition += ( pSmallSurface->pitch * (py+ncy) ); //offset by y
@@ -197,14 +262,14 @@ for (int ncx=0; ncx<4; ncx++){
   Uint32 newcol = SDL_MapRGB ( pSmallSurface->format , r , g , b ) ;
   memcpy ( pPosition , &newcol , pSmallSurface->format->BytesPerPixel ) ;
 }}
-            fy -= dy*4;
+            fy -= dy*LOWQSTEP;
         }
-        fx += dx*4;
+        fx += dx*LOWQSTEP;
     }
 }
 
 int *arrHigher=NULL;
-#define HQSTEPSIZE 1
+#define HQSTEPSIZE 2
 void DrawMenagerieHigherQuality( SDL_Surface* pSmallSurface, PhasePortraitSettings*settings) 
 {
 	if (arrHigher==NULL)
@@ -277,3 +342,4 @@ void BlitMenagerie(SDL_Surface* pSurface,SDL_Surface* pSmallSurface)
 	dest.h = PlotHeight; 
 	SDL_BlitSurface(pSmallSurface, &src, pSurface, &dest);
 }
+
