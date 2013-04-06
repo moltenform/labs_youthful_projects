8f6c2f3de60a5787693897ac5f12a18a056bb9b4
ben fisher <amoltenform@gmail.com>
2013-04-05 23:18:02 -0700

    updating onlineplot

--- src/chaos_cs/Cs/CsGraphingCalc/htmlplot/onlineplot/plotmydata2.js
+++ src/chaos_cs/Cs/CsGraphingCalc/htmlplot/onlineplot/plotmydata2.js
@@ -20,30 +20,34 @@ function createDataFromString(sIn)
 {
 	var ar = sIn.replace(/\r\n/g,'\n').split('\n');
 	var data = [];
-     
-     for (var i=0; i<ar.length; i++)
-     {
-	    var s = ar[i];
-	    //~ var s = ar[i].replace(/ /g,'').replace(/\t/g,'');
-	    if (!s) continue;
-	    //split on comma or on tab
-	    if (s.indexOf(',')!=-1)
-	    {
-	    var spl = s.split(',');
-	    data.push( {x:parseFloat(spl[0]), y:parseFloat(spl[1])} );
-	    }
-	    else if (s.indexOf('\t')!=-1)
-	    {
-	    var spl = s.split('\t');
-	    data.push( {x:parseFloat(spl[0]), y:parseFloat(spl[1])} );
-	    }
-	    else
-	    {
-	    data.push( {x: i + 0.0, y:parseFloat(s) } );
-	    }
+	
+	for (var i=0; i<ar.length; i++)
+	{
+		var s = ar[i];
+		//~ var s = ar[i].replace(/ /g,'').replace(/\t/g,'');
+		if (!s) continue;
+		//split on comma or on tab
+		if (s.indexOf(',')!=-1)
+		{
+			var spl = s.split(',');
+			data.push( {x:parseFloat(spl[0]), y:parseFloat(spl[1])} );
+		}
+		else if (s.indexOf('\t')!=-1)
+		{
+			var spl = s.split('\t');
+			data.push( {x:parseFloat(spl[0]), y:parseFloat(spl[1])} );
+		}
+		else
+		{
+			data.push( {x: i + 0.0, y:parseFloat(s) } );
+		}
+		
 		if (isNaN(data[data.length-1].x)||isNaN(data[data.length-1].y))
 			data.pop(); //don't want NaNs
-     }
-     return data;
+	}
+	
+	function cmpdata(d1,d2) {return d1.x - d2.x;}
+	data.sort(cmpdata);
+	return data;
 }
 
